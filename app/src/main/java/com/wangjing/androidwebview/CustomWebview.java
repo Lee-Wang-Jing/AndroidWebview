b59775cbcc13a497771520d09cb6f198df0962c2
919335417@qq.com <919335417@qq.com>
2018-06-07 16:46:08 +0800

    修复setLoadWithOverviewMode为true后在某些手机上面打开Webview会变形，比如oppo 5.1系统，默认设置成false 新版本0.0.11发布

--- app/src/main/java/com/wangjing/androidwebview/CustomWebview.java
+++ app/src/main/java/com/wangjing/androidwebview/CustomWebview.java
@@ -151,7 +151,8 @@ public class CustomWebview extends WebView {
         //设置为false，设置true，某些手机上某些情况会崩溃 https://bugly.qq.com/v2/crash-reporting/crashes/41f89fb766/7869?pid=1
         webSettings.setSupportMultipleWindows(false);
         //99是否允许WebView度超出以概览的方式载入页面，默认false。即缩小内容以适应屏幕宽度。该项设置在内容宽度超出WebView控件的宽度时生效，例如当getUseWideViewPort() 返回true时。
-        webSettings.setLoadWithOverviewMode(true);
+//        setLoadWithOverviewMode为true后在某些手机上面打开Webview会变形，比如oppo 5.1系统
+//        webSettings.setLoadWithOverviewMode(true);
         webSettings.setAppCacheEnabled(true);
         webSettings.setDatabaseEnabled(true);
         webSettings.setDomStorageEnabled(true);
